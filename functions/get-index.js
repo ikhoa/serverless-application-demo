8aed9bc4b7d13674fddd89eb33f70fabcd8d6df6
Kelly Audu <kelly.audu@hotmail.com>
2020-05-17 18:25:07 +0100

    =Secured API With IAM Authorization

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,6 +1,8 @@
 const fs = require("fs")
 const Mustache = require('mustache')
+const aws4 = require('aws4')
 const http = require('superagent-promise')(require('superagent'), Promise)
+const URL = require('url')
 
 const restaurantsApiRoot = process.env.restaurants_api
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -18,7 +20,20 @@ function loadHtml () {
 }
 
 const getRestaurants = async () => {
-  return (await http.get(restaurantsApiRoot)).body
+  const url = URL.parse(restaurantsApiRoot)
+  const opts = {
+    host: url.hostname,
+    path: url.pathname
+  }
+  aws4.sign(opts)
+
+  return (
+    await http.get(restaurantsApiRoot)
+      .set('Host', opts.headers['Host'])
+      .set('X-Amz-Date', opts.headers['X-Amz-Date'])
+      .set('Authorization', opts.headers['Authorization'])
+      .set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token'])
+  ).body
 }
 
 module.exports.handler = async () => {
